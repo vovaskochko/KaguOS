949938d50a0894fcdd3167f2fd80320b9131ec8f
Volodymyr Skochko <vovaskochko@gmail.com>
2024-02-14 16:50:02 +0200

    KaguOS. Lesson 2.2: Syntax sugar for assignment and memory copy.
    * Support of literal string assignment *SOME_ADDRESS="some val" was implemented in compiler.
    * Support of memory copy *SOME_ADDRESS=*ANOTHER_ADDRESS was implemented in compiler.
    
    Tasks:
    * Extend compiler to support increment and decrement operators.
    * Optionally: introduce +  operator to support code lines of the form *SOME_ADDRESS_OUT=*SOME_ADDRESS1 + *SOME_ADDRESS2 with variable count of spaces near + symbol.

--- src/kernel_base.sh
+++ src/kernel_base.sh
@@ -4,14 +4,14 @@
 # INITRAMFS_START                        #
 ##########################################
 # Write help info to RAM to simplify debugging.
-write_to_address ${GLOBAL_DISPLAY_INFO_ADDRESS} "${GLOBAL_DISPLAY_INFO}"
-write_to_address ${GLOBAL_INPUT_INFO_ADDRESS} "${GLOBAL_INPUT_INFO}"
-write_to_address ${GLOBAL_ARGS_INFO_ADDRESS} "${GLOBAL_ARGS_INFO}"
-write_to_address ${GLOBAL_OUTPUT_INFO_ADDRESS} "${GLOBAL_OUTPUT_INFO}"
-write_to_address ${GLOBAL_COMPARE_RESULT_INFO_ADDRESS} "${GLOBAL_COMPARE_RESULT_INFO}"
-write_to_address ${GLOBAL_NEXT_CMD_INFO_ADDRESS} "${GLOBAL_NEXT_CMD_INFO}"
+*GLOBAL_DISPLAY_INFO_ADDRESS="${GLOBAL_DISPLAY_INFO}"
+*GLOBAL_INPUT_INFO_ADDRESS="${GLOBAL_INPUT_INFO}"
+*GLOBAL_ARGS_INFO_ADDRESS="${GLOBAL_ARGS_INFO}"
+*GLOBAL_OUTPUT_INFO_ADDRESS="${GLOBAL_OUTPUT_INFO}"
+*GLOBAL_COMPARE_RESULT_INFO_ADDRESS="${GLOBAL_COMPARE_RESULT_INFO}"
+*GLOBAL_NEXT_CMD_INFO_ADDRESS="${GLOBAL_NEXT_CMD_INFO}"
 
-write_to_address ${GLOBAL_DISPLAY_ADDRESS} "RAMFS init - done."
+*GLOBAL_DISPLAY_ADDRESS="RAMFS init - done."
 display_success
 ##########################################
 # INITRAMFS_END                          #
@@ -23,7 +23,7 @@ display_success
 ##########################################
 
 # Display welcome message:
-write_to_address ${GLOBAL_DISPLAY_ADDRESS} "Welcome to KaguOS"
+*GLOBAL_DISPLAY_ADDRESS="Welcome to KaguOS"
 display_success
 
 # NOTE AI: Ask AI assistant about labels and goto instruction in C language.
@@ -31,12 +31,12 @@ LABEL:kernel_loop_start
 
 
 # Display prompt to enter the value:
-write_to_address ${GLOBAL_DISPLAY_ADDRESS} " :) "
+*GLOBAL_DISPLAY_ADDRESS=" :) "
 display_print
 
 # read cmd from keyboard and split into command and arguments:
 read_input
-write_to_address ${GLOBAL_ARG1_ADDRESS} "1"
+*GLOBAL_ARG1_ADDRESS="1"
 cpu_execute "${CPU_GET_COLUMN_CMD}" ${GLOBAL_INPUT_ADDRESS} ${GLOBAL_ARG1_ADDRESS}
 
 
@@ -45,7 +45,7 @@ println("Parsed command:")
 println(*GLOBAL_OUTPUT_ADDRESS)
 
 # check for exit command:
-write_to_address ${GLOBAL_ARG1_ADDRESS} "exit"
+*GLOBAL_ARG1_ADDRESS="exit"
 cpu_execute "${CPU_EQUAL_CMD}" ${GLOBAL_OUTPUT_ADDRESS} ${GLOBAL_ARG1_ADDRESS}
 jump_if ${LABEL_kernel_terminate}
 
@@ -62,7 +62,7 @@ jump_to ${LABEL_kernel_loop_start}
 # termination code
 LABEL:kernel_terminate
 # Display goodbye message:
-write_to_address ${GLOBAL_DISPLAY_ADDRESS} "Goodbye!"
+*GLOBAL_DISPLAY_ADDRESS="Goodbye!"
 display_success
 jump_to ${GLOBAL_TERMINATE_ADDRESS}
 
