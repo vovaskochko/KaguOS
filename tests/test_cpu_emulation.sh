750ee09d8c2eb8cec0a9151c51a25f8c72f829fe
Volodymyr Skochko <vovaskochko@gmail.com>
2025-03-05 13:36:43 +0200

    add more unit tests for is_num check

--- tests/test_cpu_emulation.sh
+++ tests/test_cpu_emulation.sh
@@ -2,7 +2,7 @@
 
 BASE_DIR="$(dirname "$0")"
 ASM=$(realpath "${BASE_DIR}/../asm.sh")
-if [ "$CPP_BOOTLOADER" -eq 1 ]; then
+if [ "$CPP_BOOTLOADER" = 1 ]; then
     BOOTLOADER=$(realpath "${BASE_DIR}/../bootloader")
     FLAGS="500"
 else
@@ -19,7 +19,7 @@ if [ ! -f "${BOOTLOADER}" ]; then
     exit 1
 fi
 
-for FILE in $(ls "${BASE_DIR}"/cpu/test_*.kga); do
+for FILE in $(ls "${BASE_DIR}"/cpu/test_is_*.kga); do
     echo "Testing $FILE"
     (DEBUG_INFO=0 "${ASM}" tests/kagu_test.kga $FILE && "${BOOTLOADER}" build/kernel.disk $FLAGS) | grep "Total tests\|Successful tests\|Failed tests" | ( (grep -B 2 "Failed tests" && echo -e "\e[41mFAILED\e[0m") || echo -e "\e[42mPASSED\e[0m")
     echo "---------------------------------------------"
